6e06f509ca46f80293ae1abf24c5dd104afa7aa3
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-05-09 22:47:29 -0400

    basic functionality of BMI in react workign and min styles

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ const App: React.FC = () => {
   return (
     <IonApp>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar color="primary">
           <IonTitle>BMI Calculator</IonTitle>
         </IonToolbar>
       </IonHeader>
